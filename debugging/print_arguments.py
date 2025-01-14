7330a6dc9d9feefbd7b059586f0cf21aac0937d8
xhoi700 <10057@holbertonstudents.com>
2025-01-14 09:48:50 +0000

    print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-for arg in sys.argv:
-    print(arg)
+if len(sys.argv) == 1:
+    print("No arguments were provided.")
+else:
+    for i in range(1, len(sys.argv)):  # Start from 1 to skip the script name
+        print(sys.argv[i])
